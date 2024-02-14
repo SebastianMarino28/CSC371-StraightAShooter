3b7ebca56a1ab220e4db4ea8ee6f136934e85888
Sebastian <sebastianchasmarino@gmail.com>
2024-02-13 19:33:34 -0800

    updated projectile direction

--- Assets/Scripts/WeaponHandler.cs
+++ Assets/Scripts/WeaponHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class WeaponHandler : MonoBehaviour
 {
     public GameObject firePoint;
+    public GameObject centerPoint;
     [Header("Bullet")]
     public GameObject bullet;
     public AudioSource bulletSound;
@@ -36,8 +37,17 @@ public class WeaponHandler : MonoBehaviour
             {
                 Instantiate(bulletSound);
             }
+
+            // calculate direction
+            Vector3 forceDirection = centerPoint.transform.forward;
+            RaycastHit hit;
+            if (Physics.Raycast(centerPoint.transform.position, centerPoint.transform.forward, out hit))
+            {
+                forceDirection = (hit.point - firePoint.transform.position).normalized;
+            }
+
             Rigidbody rb = Instantiate(bullet, firePoint.transform.position, transform.rotation).GetComponent<Rigidbody>();
-            rb.velocity = transform.forward * bulletSpeed;
+            rb.velocity = forceDirection * bulletSpeed;
             canFireBullet = false;
             StartCoroutine(ResetCooldown(ptype));
         }
