53bee70c717c186614c4fb3f42738befe5815130
Joshua Levin <thejoryle@gmail.com>
2024-03-20 11:18:40 -0700

    replaced upgrade block with lightbulb

--- Assets/Scripts/UpgradeBehaviour.cs
+++ Assets/Scripts/UpgradeBehaviour.cs
@@ -10,11 +10,25 @@ public class UpgradeBehaviour : MonoBehaviour
     private SFXManager sfxManager;
     public InputActionReference pauseAction;
 
+    [Header("Float behaviour")]
+    public float heightOffset;
+    public float moveSpeed;
+    private float moveDir = 1;
+
     public void Awake() {
         anim = GameObject.FindGameObjectWithTag("UpgradeScreen").GetComponent<Animator>();
         sfxManager = GameObject.Find("SFXManager").GetComponent<SFXManager>();
     }
-    
+
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + (moveSpeed * moveDir * Time.deltaTime), transform.position.z);
+        if (transform.position.y >= heightOffset || transform.position.y <= -heightOffset)
+        {
+            moveDir *= -1;
+        }
+    }
+
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Player")) {
             sfxManager.playAha();
